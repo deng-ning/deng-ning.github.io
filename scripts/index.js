7125eea47edd72d313d4d0eed14bc0a708793209
deng-ning <dengning911@gmail.com>
2021-02-01 23:03:05 -0700

    combined home and color flipper

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-const colors = ["green", "red", "blue", "rgba(133,122,200)", "#f15025"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', function(){
-    const getANumber = Math.floor(Math.random() * colors.length);
-    color.textContent = colors[getANumber];
-    document.body.style.backgroundColor = colors[getANumber];
-});
+btn.addEventListener('click', function(){changeColor()});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+function changeColor() {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+    // console.log(hexColor);
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+}
