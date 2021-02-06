f67edc6166e62ff2f009cd44cd77c1382ce174b9
deng-ning <dengning911@gmail.com>
2021-02-06 00:47:32 -0700

    added reviews, moved bg color to counter

--- scripts/counter.js
+++ scripts/counter.js
@@ -23,6 +23,23 @@ btns.forEach(function (btn) {
     });
 });
 
-function updateCounter(){
-    
+// backgroud color
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){changeColor()});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+function changeColor() {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+    // console.log(hexColor);
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
 }
