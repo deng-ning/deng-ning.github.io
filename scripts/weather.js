5bda1c3e5eef12ec79db7f5044bf207ad33d8685
deng-ning <dengning911@gmail.com>
2021-02-06 01:18:04 -0700

    http->https

--- scripts/weather.js
+++ scripts/weather.js
@@ -14,7 +14,7 @@ btn.addEventListener('click', function(){
     } else {
         whose.textContent = "My";
     }
-    fetch('http://dataservice.accuweather.com/currentconditions/v1/locationKey?locationKey=346936&apikey=qG5TAKJCAWfy6ZAQ49w8Vz0wBmLvTPi7')
+    fetch('https://dataservice.accuweather.com/currentconditions/v1/locationKey?locationKey=346936&apikey=qG5TAKJCAWfy6ZAQ49w8Vz0wBmLvTPi7')
     .then(response => response.json())
     .then(json => {
         console.log(json);
